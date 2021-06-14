0dadd365e9ca9ba9742166b218bd0f4f56428b9e
gk-j2 <ger.ka.v@yandex.ru>
2021-06-14 12:46:31 +0500

    Rewrite with class

--- src/web_calc.py
+++ src/web_calc.py
@@ -5,62 +5,80 @@ from werkzeug.exceptions import abort
 
 appcalc = Flask(__name__)
 
-def check_input_num(num):
-    if not isinstance(num, (int, float)):
-        return 0
-    return 1
-
-def check_operation(operation):
-    allowed = ["+","-","*","/","//","%","**"]
-    if operation not in allowed:
-        return 0
-    return 1
-
-def calculate(file_json):
-    check_a = check_input_num(file_json['a'])
-    check_b = check_input_num(file_json['b'])
-    check_oper = check_operation(file_json['operation'])
-    if check_a and check_b and check_oper:
-        a = file_json['a']
-        b = file_json['b']
-        operation = file_json['operation']
-        if operation == "+":
-            return a+b
-        elif operation == "-":
-            return a-b
-        #elif operation == "*":
-        #    return a*b
-        elif operation == "**":
-            if b >= 0:
-                return a**int(b)
-            else:
-                return "Error! <b> be greater than or equal to zero!"   
-        elif operation == "/":
-            if b != 0:
-                return a/b
-            else:
-                return "Error! Division by zero!"
-        elif operation == "//":
-            if b != 0:
-                return a//b
-            else:
-                return "Error! Division by zero!"
-        elif operation == "%":
-            if b != 0:
-                return a%b
-            else:
-                return "Error! Division by zero!"
+def is_num(param1, param2) -> bool:
+    if isinstance(param1, (int, float)) and isinstance(param2, (int, float)):
+        return True
     else:
-        return ("Error! Input argument is not valide!")
+        return False
 
+def is_oper(operation) -> bool:
+    allowed = ["+","-","*","/","//","%","**"]
+    if operation in allowed:
+        return True
+    else:
+        return False
 
-@appcalc.route('/', methods=['POST'])
+def is_good_input(param1, param2, param3) -> bool:
+    if is_num(param1, param2) and is_oper(param3):
+        return True
+    else:
+        return False
+
+class Calculator(object):
+    def __init__(self, num1, num2, oper):
+        self.num1 = num1
+        self.num2 = num2
+        self.oper = oper
+
+    def calculate(self):
+        if self.oper == "+":
+            return self.num1 + self.num2
+
+        elif self.oper == "-":
+            return self.num1 - self.num2
+
+        elif self.oper == "*":
+            return self.num1 * self.num2
+
+        elif self.oper == "**":
+            if self.num2 >= 0:
+                return self.num1 ** int(self.num2)
+            else:
+                return "Error! <b> must be greater than or equal to zero!" 
+
+        elif self.oper == "/":
+            if self.num2 != 0:
+                return self.num1 / self.num2
+            else:
+                return "Error! Division by zero!"
+
+        elif self.oper == "//":
+            if self.num2 != 0:
+                return self.num1 // self.num2
+            else:
+                return "Error! Division by zero!"
+
+        elif self.oper == "%":
+            if self.num2 != 0:
+                return self.num1 % self.num2
+            else:
+                return "Error! Division by zero!"
+
+@appcalc.route('/calc/my_api/v1/', methods=['POST'])
 def send_answer():
     if not request.json or (not 'a' in request.json and not 'b' in request.json and not 'operation' in request.json):
-        abort(404)
-    result = calculate(request.json)
-    answer = {'answer': result}
-    return answer, 201
+        abort(400)
+    param1 = request.json['a']
+    param2 = request.json['b']
+    param3 = request.json['operation']
+    if is_good_input(param1, param2, param3):
+        calc = Calculator(param1, param2, param3)
+        result = calc.calculate()
+        answer = {'answer': result}
+        return answer, 201
+    else:
+        mes = {'answer': "Error! Input argument is not valide!"}
+        return mes, 201
 
 if __name__ == '__main__':
     appcalc.run(host='0.0.0.0', port='80')
